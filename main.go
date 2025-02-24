79f24554e6695e50061b60742b3a3387cef8e701
1vanopolos <prichak7@gmail.com>
2025-02-24 11:16:46 +0300

    naming

--- main.go
+++ main.go
@@ -21,10 +21,10 @@ func main() {
 	if errEnv != nil {
 		log.Fatal("Ошибка при загрузке .env file")
 	}
-	PORT := os.Getenv("TODO_PORT")
-	DBFILE := os.Getenv("TODO_DBFILE")
+	port := os.Getenv("TODO_PORT")
+	todo_db := os.Getenv("TODO_DBFILE")
 
-	db, err := sql.Open("sqlite", DBFILE)
+	db, err := sql.Open("sqlite", todo_db)
 	if err != nil {
 		log.Println(err)
 		return
@@ -36,7 +36,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	dbFile := filepath.Join(appPath, DBFILE)
+	dbFile := filepath.Join(appPath, todo_db)
 	_, err = os.Stat(dbFile)
 
 	var install bool
@@ -62,8 +62,8 @@ func main() {
 	router.Post("/api/task/done", func(w http.ResponseWriter, req *http.Request) { handlers.DoneTask(w, req, db) })
 	router.Delete("/api/task", func(w http.ResponseWriter, req *http.Request) { handlers.DeleteTask(w, req, db) })
 
-	fmt.Println("Сервер прослушивает порт", PORT)
-	if err := http.ListenAndServe(PORT, router); err != nil {
+	fmt.Println("Сервер прослушивает порт", port)
+	if err := http.ListenAndServe(port, router); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
 		return
 	}
